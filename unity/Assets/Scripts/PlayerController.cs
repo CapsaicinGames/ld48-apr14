f7a2815c6cb2d2742aa57a3780bbb2ae2ac41700
Stephen <widget@stdin.co.uk>
2014-04-26 11:18:35 +0100

    Adding controllable shark/player object to Sea scene

---
+++ unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float steeringSensitivity;
+    public float speedScalar;
+    public float minSteeringScalar;
+    public float maxSteeringScalar;
+    public float speedSteeringFactor;
+
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        float forward = Input.GetAxis("Vertical");
+        float steering = Input.GetAxis("Horizontal");
+
+        rigidbody.AddRelativeForce(0f, 0f, forward * speedScalar);
+
+        float currentSpeedMultiplier = (float)rigidbody.velocity.magnitude;
+        if (currentSpeedMultiplier < minSteeringScalar)
+        {
+            currentSpeedMultiplier = minSteeringScalar;
+        }
+        else if (currentSpeedMultiplier > maxSteeringScalar)
+        {
+            currentSpeedMultiplier = maxSteeringScalar;
+        }
+
+        currentSpeedMultiplier *= speedSteeringFactor;
+
+        rigidbody.AddRelativeTorque(0f, steering * steeringSensitivity * currentSpeedMultiplier, 0);
+	}
+}
