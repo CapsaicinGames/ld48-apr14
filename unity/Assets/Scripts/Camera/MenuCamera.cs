26b590b8b58261945a676ad7ab53360c1df34a86
SmallGods <c.d.gibb@gmail.com>
2014-04-26 23:58:36 +0100

    Added NGUI resources

---
+++ unity/Assets/Scripts/Camera/MenuCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using CapsaicinGames.Shark;
+
+public class MenuCamera : MonoBehaviour {
+
+    public GameObject endGameCamera;
+    public SwimmerSpawner swimmerSpawner;
+
+    private SharkInput _sharkInput;
+
+	// Use this for initialization
+	void Start () {
+        _sharkInput = FindObjectOfType<SharkInput>();
+        swimmerSpawner.OnEndGameCallback += swimmerSpawner_OnEndGameCallback;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void swimmerSpawner_OnEndGameCallback()
+    {
+        _sharkInput.enabled = false;
+        endGameCamera.SetActive(true);
+        swimmerSpawner.OnEndGameCallback -= swimmerSpawner_OnEndGameCallback;
+    }
+
+    private void Restart()
+    {
+        SceneControl.RestartGame();
+    }
+}
