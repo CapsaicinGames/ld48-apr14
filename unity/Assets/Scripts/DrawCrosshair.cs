169f8eec58f3ce456c1a3f9efb55ac9384420d00
Stephen <widget@stdin.co.uk>
2014-04-27 11:40:56 +0100

    Adding initial version of crosshair

---
+++ unity/Assets/Scripts/DrawCrosshair.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawCrosshair : MonoBehaviour 
+{
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    /*(if (transform.parent.position.y < 0)
+        {
+            this.enabled = true;
+        }
+        else
+        {
+            this.enabled = false;
+        }*/
+	}
+}
