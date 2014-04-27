cfb39650d27d1a08afc458345adf61ec3d6776e9
Stephen <widget@stdin.co.uk>
2014-04-27 02:12:38 +0100

    Adding BackgroundAudio component to Player which plays background audio

--- unity/Assets/Scripts/PlayerBackgroundAudio.cs
+++ unity/Assets/Scripts/PlayerBackgroundAudio.cs
@@ -14,17 +14,21 @@ public class PlayerBackgroundAudio : MonoBehaviour
         float ypos = transform.parent.transform.position.y;
         float spdSqr = transform.parent.rigidbody.velocity.sqrMagnitude;
         float spdFactor = Mathf.InverseLerp(0, 200, spdSqr);
-        float volume = Mathf.InverseLerp(-3, 0, ypos);
+        float yFactor = Mathf.InverseLerp(-3, 0, ypos);
+        float volume;
 
-        volume *= spdFactor;
         if (ypos > 0.5)
         {
             volume = 0;
+        }
+        else
+        {
+            volume = yFactor * spdFactor;
         }
         finSound.volume = volume;
         finSound.pitch = 1 + (spdFactor / 3);
 
         spdFactor = Mathf.InverseLerp(0, 64, spdSqr);
-        idleSound.volume = 0.5f - (spdFactor/2f);
+        idleSound.volume = yFactor * (0.5f - (spdFactor/2f));
 	}
 }
